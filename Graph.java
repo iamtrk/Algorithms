56d999951beb83118eda2122b32722c9dd211a60
Ravikumar <ravikumar.ee@gmail.com>
2017-03-27 10:06:13 +0530

    Graph algo

--- Graph.java
+++ Graph.java
@@ -9,23 +9,23 @@ public class Graph {
 
     private Map<String, List<String>> graph;
     private Map<String, Boolean> visited;
-    private String path = "";
 
-    public testingal() {
+    public Graph() {
         graph = new HashMap<>();
         visited = new HashMap<>();
     }
 
     public static void main(String[] args) {
-        testingal r = new testingal();
-        r.addEdge("A", "B");
-        r.addEdge("B", "C");
-        r.addEdge("C", "D");
-        r.addEdge("D", "A");
+        Graph graph = new Graph();
+        graph.addEdge("A", "B");
+        graph.addEdge("B", "C");
+        graph.addEdge("C", "D");
+        graph.addEdge("D", "A");
 
         String source = "A";
         String dest   = "D";
-        r.DFS(source, dest, source);
+        graph.BFS(source, dest, source);
+        graph.DFS(source, dest, source);
 
     }
 
@@ -59,7 +59,7 @@ public class Graph {
 
         graph.put(B, b);
     }
-
+    // DFS With path tracking capabilities
     public void DFS( String source, String dest, String path) {
 
         if(source.equals(dest)) {
@@ -75,9 +75,36 @@ public class Graph {
                 DFS(a, dest, path+" => "+a);
             }
          }
+    }
+    // BFS With path tracking capabilities
+    public void BFS(String source, String dest, String path) {
+        Queue<KV> queue = new LinkedList<>();
+        queue.add(new KV(source, path));
+        visited.put(source, true);
+        while (!queue.isEmpty()) {
+            KV vert = queue.poll();
+            if(dest.equals(vert.key)) {
+                System.out.println(vert.value);
+                return;
+            }
 
+            for (String a : graph.get(vert.key)) {
+                if(visited.get(a)==null || !visited.get(a)) {
+                    visited.put(a, true);
+                    queue.add(new KV(a, vert.value+" => "+a));
+                }
+            }
+        }
+    }
+    // Class used to keep track of path in BFS
+    class KV {
+        String key;
+        String value;
 
-
+        public KV(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 }
 
