1ace9c06ddd675c48ea6e3a21f2a0432a79a6f8c
Ravikumar <ravikumar.ee@gmail.com>
2017-12-26 08:13:55 +0530

    Update reverse-linkedlist.java

--- linkedlist/reverse-linkedlist.java
+++ linkedlist/reverse-linkedlist.java
@@ -3,12 +3,15 @@ public void reverse() {
 }
 
 public Node reverse(Node node) {
+	// for a null node or single node, return the same node.
 	if(node==null || node.next==null) {
 		return node;
 	}
-
+	// Reverse the list starting from the next node.
 	Node rev = reverse(node.next);
+	// Just change the pointers.
 	node.next.next = node;
 	node.next = null;
+	// return the reversed node.
 	return rev;
 }
