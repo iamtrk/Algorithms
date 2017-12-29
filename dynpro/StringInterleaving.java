fb133a152026813452dc9ee2fb9496c1e48646f4
Ravikumar <ravikumar.ee@gmail.com>
2017-12-29 11:34:54 +0530

    Create StringInterleaving.java

---
+++ dynpro/StringInterleaving.java
@@ -0,0 +1,37 @@
+public class stringInterleaving {
+    public static void main(String[] args) {
+        String a = "imt";
+
+        String b = "akr";
+
+        String c = "iamtrk";
+
+
+        boolean[][] intr = new boolean[a.length()+1][b.length()+1];
+        intr[0][0] = true;
+
+        for (int i=1; i<a.length()+1; i++) {
+            if(a.charAt(i-1) == c.charAt(i-1)) {
+                intr[i][0] = intr[i-1][0];
+            }
+        }
+
+        for(int j=1; j<b.length()+1; j++) {
+            if(b.charAt(j-1) == c.charAt(j-1)) {
+                intr[0][j] = intr[0][j-1];
+            }
+        }
+
+        for (int i=1; i<a.length()+1; i++) {
+            for (int j=1; j<b.length()+1; j++) {
+                if(a.charAt(i-1) == c.charAt(i+j-1)) {
+                    intr[i][j] = intr[i-1][j];
+                } else if(b.charAt(j-1) == c.charAt(i+j-1)) {
+                    intr[i][j] = intr[i][j-1];
+                } else intr[i][j] = false;
+            }
+        }
+
+        System.out.println(intr[a.length()][b.length()]);
+    }
+}
