b8faa0e1d488cfacfae2e8ef8ee783e5c917f53c
Ravikumar <ravikumar.ee@gmail.com>
2017-12-31 09:54:47 +0530

    Update NumBinaryTrees.java

--- dynpro/NumBinaryTrees.java
+++ dynpro/NumBinaryTrees.java
@@ -1,1 +1,18 @@
-Given a post order elements of a tree, Number of possible binary trees are Catalan number.
+//Given a post order elements of a tree, Number of possible binary trees are Catalan number.
+
+public class Solution {
+    public int numTrees(int A) {
+        int[] res = new int[A+1];
+        
+        res[0] = 1;
+        res[1] = 1;
+        
+        for(int i=2; i<=A; i++) {
+            for(int j=0; j<i; j++) {
+                res[i] = res[i] + res[j]*res[i-j-1];
+            }
+        }
+        
+        return res[A];
+    }
+}
