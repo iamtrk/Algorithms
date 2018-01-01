cb53186394e6a434abeaaf5d7139d6bf6a36cd2f
Ravikumar <ravikumar.ee@gmail.com>
2018-01-01 13:33:00 +0530

    Create SmallestMissingPositiveNumber.java

---
+++ ArrayAlgos/SmallestMissingPositiveNumber.java
@@ -0,0 +1,16 @@
+public class SmallestMissingPositiveNumber {
+    public static void main(String[] args) {
+        int[] an = {4,3,7,8,1};
+        
+        for (int i = 0; i<an.length; i++) {
+
+            if(Math.abs(an[i]-1)<an.length && an[Math.abs(an[i]-1)]>0) {
+                an[an[i]-1] = -1*an[an[i]-1];
+            }
+        }
+
+        for (int m : an) {
+            System.out.print(m+" ");
+        }
+    }
+}
