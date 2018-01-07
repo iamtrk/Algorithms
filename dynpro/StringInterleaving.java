9ac1d5de3aec20928d6926a80cbfe172cd531b3f
Ravikumar <ravikumar.ee@gmail.com>
2018-01-07 11:39:07 +0530

    Update StringInterleaving.java

--- dynpro/StringInterleaving.java
+++ dynpro/StringInterleaving.java
@@ -24,10 +24,12 @@ public class stringInterleaving {
 
         for (int i=1; i<a.length()+1; i++) {
             for (int j=1; j<b.length()+1; j++) {
-                if(a.charAt(i-1) == c.charAt(i+j-1)) {
+                if(a.charAt(i-1) == c.charAt(i+j-1) && b.charAt(j-1) != c.charAt(i+j-1)) {
                     intr[i][j] = intr[i-1][j];
-                } else if(b.charAt(j-1) == c.charAt(i+j-1)) {
+                } else if(a.charAt(i-1) != c.charAt(i+j-1) && b.charAt(j-1) == c.charAt(i+j-1)) {
                     intr[i][j] = intr[i][j-1];
+                } else if(a.charAt(i-1) == c.charAt(i+j-1) && b.charAt(j-1) == c.charAt(i+j-1)) {
+                    intr[i][j] = intr[i][j-1] || intr[i-1][j];
                 } else intr[i][j] = false;
             }
         }
