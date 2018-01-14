607f37991c3c0cabbff2fd8787662616696ae70b
Ravikumar <ravikumar.ee@gmail.com>
2018-01-14 12:48:01 +0530

    Update NextGreaterElement.java

--- ArrayAlgos/NextGreaterElement.java
+++ ArrayAlgos/NextGreaterElement.java
@@ -1,62 +1,49 @@
 public class NextGreaterElement {
-
     public static void main(String[] args) {
+        int[] array = {1,2,3};
 
-// TODO Auto-generated method stub
-
-        int[] numbers = {2,4,5,3};
-
-        if(numbers == null || numbers.length == 0)
-
-            return;
-
-        int[] tempoutput = nges(numbers);
-
-        for (int aTempoutput1 : tempoutput) {
-            System.out.print(aTempoutput1 + " ");
-        }
-
-        System.out.println();
-
-        int i=0;
-        for(; i< tempoutput.length-1; i++) {
-            if(tempoutput[i] == numbers[i]) {
-                tempoutput[i] = -1;
-            }
-        }
-
-        tempoutput[i] = -1;
-
-        for (int aTempoutput : tempoutput) {
-            System.out.print(aTempoutput + " ");
-        }
-        System.out.println();
-
+        do {  // Must start at lowest permutation
+            System.out.println(Arrays.toString(array));
+        } while (nextPermutation(array));
     }
 
-    public static int[] nges(int[] numbers) {
+    static boolean nextPermutation(int[] array) {
+        // Find longest non-increasing suffix.
+        // Example of a non-increasing sequence in 9,5,8,4,2,1 is => 8,4,2,1
+        int i = array.length - 1;
+        // this loop breaks when array[i]>array[i-1]
+        while (i > 0 && array[i - 1] >= array[i])
+            i--;
+        // Now i is the head index of the suffix i.e at 8.
 
-        int[] output = new int[numbers.length];
+        // Are we at the last permutation already?
+        if (i == 0)
+            return false;
 
-        int nextmax = 0;
-        for (int i = numbers.length - 1; i >= 0; i--) {
+        // Let array[i - 1] be the pivot
+        // Find rightmost element that exceeds the pivot
+        int j = array.length - 1;
+        while (array[j] <= array[i - 1])
+            j--;
+        // Now the value array[j] will become the new pivot
+        // Assertion: j >= i
 
-            if (i == numbers.length - 1) {
-                output[i] = numbers[i];
-                nextmax = output[i];
-            } else {
-                if (numbers[i + 1] > numbers[i]) {
-                    output[i] = numbers[i + 1];
-                } else {
-                    output[i] = nextmax;
-                }
-                
-                if (numbers[i] > nextmax) {
-                    output[i] = numbers[i];
-                    nextmax = output[i];
-                }
-            }
+        // Swap the pivot with j
+        int temp = array[i - 1];
+        array[i - 1] = array[j];
+        array[j] = temp;
+
+        // Reverse the suffix
+        j = array.length - 1;
+        while (i < j) {
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            i++;
+            j--;
         }
-        return output;
+
+        // Successfully computed the next permutation
+        return true;
     }
 }
