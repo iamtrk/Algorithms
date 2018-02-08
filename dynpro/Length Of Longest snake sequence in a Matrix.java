0738e257558941b8e32dca6b91fdcb56f5d9eec2
Ravikumar <ravikumar.ee@gmail.com>
2018-02-08 11:45:30 +0530

    Update Length Of Longest snake sequence in a Matrix.java

--- dynpro/Length Of Longest snake sequence in a Matrix.java
+++ dynpro/Length Of Longest snake sequence in a Matrix.java
@@ -1,3 +1,5 @@
 
 Snake sequence can be made if number in adjacent right cell or
 number in the adjacent down cell is either +1 or –1 from the num­ber in the cur­rent cell.
+  
+  https://algorithms.tutorialhorizon.com/find-longest-snake-sequence-in-a-given-matrix/
